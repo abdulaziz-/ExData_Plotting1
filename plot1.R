734a529d98c032f4236c605caefe3b94d7d8034d
abdulaziz- <abdulaziz0@gmail.com>
2017-01-09 00:59:04 +0300

    plot1 code to read the data and construct plot 1 still some update to be made

---
+++ plot1.R
@@ -0,0 +1,10 @@
+# plot1.R is an R code that constructs plot1.png
+
+## 1. Read the data found in the current working directory
+## we assume we are at the folder that contains the file
+
+data <- read.csv(text = grep("^[1,2]/2/2007", readLines("household_power_consumption.txt"), value = TRUE), sep=";", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## 2. Generate plot 1
+
+hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
