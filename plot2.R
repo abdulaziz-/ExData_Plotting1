cb2da5c2bcbffc2b042b7735b422408024777dd1
abdulaziz- <abdulaziz0@gmail.com>
2017-01-09 01:52:24 +0300

    few changes

--- plot2.R
+++ plot2.R
@@ -3,12 +3,18 @@
 ## 1. Read the data found in the current working directory
 ## we assume we are at the folder that contains the file
 
-# only read the lines that match the date subset
+data <- read.csv("household_power_consumption.txt", header = T, sep = ';', na.strings = "?", nrows = 2075259, check.names = F, stringsAsFactors = F, comment.char = "", quote = '\"')
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
-data <- read.csv(text = grep("^[1,2]/2/2007", readLines("household_power_consumption.txt"), value = TRUE), sep=";", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+## Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Convert dates
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
+## Generating Plot 2
+
 png('plot2.png', width = 480, height = 480)
-plot(data$Global_active_power ~ data$Datetime, type = "l",ylab = "Global Active Power (kilowatts)", xlab = "")
+plot(data$Global_active_power ~ data$Datetime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
 dev.off()
